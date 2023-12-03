cb2658e0f6eb951724427071c3f65d8df2e9ee17
OptionalKarl <75976778+OptionalKarl@users.noreply.github.com>
2023-12-03 17:36:26 +0000

    Common.py script added for future use

--- common.py
+++ common.py
@@ -1,36 +1,47 @@
-from itertools import permutations
+import csv
 
-grid_size = (5, 3)
-center = (2, 1)
-target_character = '*'
-repeating_characters = '10'
-other_character = '.'
+def import_csv(filepath):
+    mylist = []
+    with open(filepath, 'r') as file:
+            for line in file:
+                mylist.append(line.strip())
+    return mylist
+def backwardsnumber(row, start):
+    number = ''
+    isnumber = True
+    while isnumber:
+        number = str(row[start]) + number
+        start -= 1
+        if start < 0 : isnumber = False
+        if not row[start].isnumeric(): isnumber = False
+    
+    return int(number)
 
-def is_valid_permutation(grid):
-    count = 0
-    for row in grid:
-        count += row.count(repeating_characters)
-        if count > 2:
-            return False
-    return count == 2
+def forwardsnumber(row,start):
+    number = ''
+    isnumber = True
+    while isnumber:
+        number = number +  str(row[start])
+        start += 1
+        if start >= len(row) : isnumber = False
+        if isnumber == True:
+            if not row[start].isnumeric(): isnumber = False
+    return int(number)
 
-def generate_permutations(grid_size, center, target_character, repeating_characters, other_character):
-    all_characters = other_character * (grid_size[0] * grid_size[1] - 3)
-    permutations_list = []
-
-    for perm in permutations(all_characters):
-        perm_list = list(perm)
-        perm_list.insert(center[0] * grid_size[1] + center[1], target_character)
-        grid = [perm_list[i:i + grid_size[1]] for i in range(0, len(perm_list), grid_size[1])]
-        if is_valid_permutation(grid):
-            permutations_list.append(grid)
-
-    return permutations_list
-
-permutations_list = generate_permutations(grid_size, center, target_character, repeating_characters, other_character)
-
-# Printing the generated valid permutations
-for grid in permutations_list:
-    for row in grid:
-        print(''.join(row))
-    print()
+def middlenumber(row,start):
+    back = start
+    forward = start + 1
+    number = ''
+    isnumber = True
+    while isnumber:
+        number = str(row[back]) + number
+        back -= 1
+        if back < 0 : isnumber = False
+        if not row[back].isnumeric(): isnumber = False
+    if forward < len(row) and row[forward].isnumeric(): isnumber = True
+    while isnumber:
+        number = number +  str(row[forward])
+        forward += 1
+        if forward > len(row) : isnumber = False
+        if not row[forward].isnumeric(): isnumber = False
+    return int(number)
