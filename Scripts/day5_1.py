c9f84b8c3fa81d303e509806aa6bf012a045adae
Karl <karl.mearns@gmail.com>
2023-12-05 14:43:42 +0000

    done

--- Scripts/day5_1.py
+++ Scripts/day5_1.py
@@ -1,36 +1,38 @@
 from common import import_csv
 def getseeds (data):
-        seeds = data[0].replace('seeds: ', '')
-        seeds = seeds.split(" ")
+        seeds = data[0].replace('seeds: ', '').split(" ")
         return seeds
-
 def getmap(mapname,data):
-    mapcomplete = False
-    startindex = data.index(mapname)
-    map1 = []
-    map2 = []
-    while mapcomplete == False:
-        convertrow = data[startindex +1]
-        if convertrow == '' : mapcomplete = True
-        else:
-                maparray = convertrow.split(" ")
-                for x in range(0, int(maparray[2])):
-                        map1.append(int(maparray[1]) + x)
-                        map2.append(int(maparray[0]) + x)
-                startindex += 1
-                if startindex == len(data) - 1: mapcomplete = True
-    maps = [map1,map2]
+    startindex = data.index(mapname) + 1
+    maps = []
+    while startindex < len(data):
+        convertrow = data[startindex]
+        if not convertrow:
+            break
+        maparray = convertrow.split(" ")
+        minval1 = int(maparray[1])
+        maxval1 = minval1 + int(maparray[2]) - 1
+        minval2 = int(maparray[0])
+        maps.append([minval1, maxval1, minval2])
+        startindex += 1
     return maps
 
-def mapper(input,map):
-    output = 0
-    input = int(input)
-    if input in map[0]:
-        outputindex = map[0].index(input)
-        output = map[1][outputindex]
-    else: output = input
-    return output   
-       
+def mapper(input_val, map_data):
+    input_val = int(input_val)
+    output = input_val
+    for grid in map_data:
+        if grid[0] <= input_val <= grid[1]:
+            output = grid[2] + (input_val - grid[0])
+            break
+    return output 
+
+def seedjourney(seed):
+    location = seed
+    for mapper_func in maps:
+        location = mapper(location, mapper_func)
+    return location
+
+
 
 file_path = 'Data/AoC2023Day5.txt'
 data = import_csv(file_path)
@@ -42,8 +44,8 @@ watertolight= getmap('water-to-light map:',data)
 lightotemp = getmap('light-to-temperature map:',data)
 temptohumidity = getmap('temperature-to-humidity map:',data)
 humiditytolocation = getmap('humidity-to-location map:',data)
-
 locationlist = []
+
 
 for seed in seedlist:
     soil = mapper(int(seed),seedtosoil)
@@ -52,10 +54,15 @@ for seed in seedlist:
     light = mapper(water,watertolight)
     temp = mapper(light,lightotemp)
     humidity = mapper(temp,temptohumidity)
-    location = mapper(humidity,humiditytolocation)
+    locationlist.append(mapper(humidity,humiditytolocation))
+
+import time
+start_time = time.time()
+
+
 
 print (min(locationlist))
-
+print("--- %s seconds ---" % (time.time() - start_time))
             
             
             
